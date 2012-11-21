b5b8365f0c6129bc1524007e4c7563519df07b07
PanteR <panter.dsd@gmail.com>
2012-11-21 23:29:35 +0400

    DiffWidget: add DiffHighlighter.

--- diff_widget.py
+++ diff_widget.py
@@ -3,6 +3,23 @@ __author__ = 'panter.dsd@gmail.com'
 
 from PySide import QtCore, QtGui
 import commit
+import re
+
+class DiffHighlighter(QtGui.QSyntaxHighlighter):
+    def __init__(self, parent = None):
+        super(DiffHighlighter, self).__init__(parent)
+
+    def highlightBlock(self, text):
+        super(DiffHighlighter, self).setFormat (0, len(text), QtCore.Qt.black)
+
+        added = re.match("^\+.*$", text)
+        if added:
+            super(DiffHighlighter, self).setFormat (added.pos, added.endpos, QtCore.Qt.green)
+
+        removed = re.match("^\-.*$", text)
+        if removed:
+            super(DiffHighlighter, self).setFormat (removed.pos, removed.endpos, QtCore.Qt.red)
+
 
 class DiffWidget(QtGui.QWidget):
     def __init__(self, path, parent = None):
@@ -11,6 +28,8 @@ class DiffWidget(QtGui.QWidget):
         self._path = path
 
         self._diff_veiw = QtGui.QPlainTextEdit(self)
+        self._diff_veiw.setWordWrapMode(QtGui.QTextOption.NoWrap)
+        diffHighlighter = DiffHighlighter(self._diff_veiw.document())
 
         layout = QtGui.QHBoxLayout()
         layout.addWidget(self._diff_veiw)
