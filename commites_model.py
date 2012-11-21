99f46276e0dc0ecd17e943bd4b02121b03873d07
PanteR <panter.dsd@gmail.com>
2012-11-21 14:35:11 +0400

    Refactoring.

--- commites_model.py
+++ commites_model.py
@@ -15,7 +15,6 @@ class Commit(object):
 
     def _commit_info(self, format_id):
         os.chdir(self._path)
-        result = []
         command = "git show -s --pretty=\"" + format_id + "\" " + self._id
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
@@ -34,6 +33,7 @@ class Commit(object):
             self.author_ = self._commit_info("%ae")
         return self.author_
 
+
 def get_commites_list(path):
     os.chdir(path)
     result = []
@@ -41,44 +41,46 @@ def get_commites_list(path):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
     for line in process.stdout.readlines():
-        result.append(Commit (path, line.strip().decode("utf-8")))
+        result.append(Commit(path, line.strip().decode("utf-8")))
     return result
+
 
 class CommitesModel(QtCore.QAbstractItemModel):
     """CommitesModel"""
-    def __init__(self, path, parent = None):
+
+    def __init__(self, path, parent=None):
         """__init__"""
         super(CommitesModel, self).__init__(parent)
         self._path = path
         self.update_commits_list()
 
     def update_commits_list(self):
-        self._commits_list = get_commites_list (self._path)
+        self._commits_list = get_commites_list(self._path)
 
-    def index(self, row, column, parent = QtCore.QModelIndex()):
+    def index(self, row, column, parent=QtCore.QModelIndex()):
         if parent.isValid():
             return QtCore.QModelIndex()
 
         return super(CommitesModel, self).createIndex(row, column)
 
-    def rowCount(self, parent = QtCore.QModelIndex()):
+    def rowCount(self, parent=QtCore.QModelIndex()):
         if parent.isValid():
             return 0
         return len(self._commits_list)
 
-    def columnCount(self, parent = QtCore.QModelIndex()):
+    def columnCount(self, parent=QtCore.QModelIndex()):
         if parent.isValid():
             return 0
         return 3
 
-    def data(self, index, role = QtCore.Qt.DisplayRole):
+    def data(self, index, role=QtCore.Qt.DisplayRole):
         if role == QtCore.Qt.DisplayRole:
             if index.column() == 0:
-                return self._commits_list[index.row()].id ()
+                return self._commits_list[index.row()].id()
             elif index.column() == 1:
-                return self._commits_list[index.row()].name ()
+                return self._commits_list[index.row()].name()
             elif index.column() == 2:
-                return self._commits_list[index.row()].author ()
+                return self._commits_list[index.row()].author()
         return None
 
     def parent(self, index):
