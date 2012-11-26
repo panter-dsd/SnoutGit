b3dd195d1175632b5c850b14928fbd393571c0a4
PanteR <panter.dsd@gmail.com>
2012-11-26 12:30:23 +0400

    Fix last commit.

--- commites_model.py
+++ commites_model.py
@@ -57,8 +57,8 @@ class CommitesModel(QtCore.QAbstractItemModel):
 
         for i in range(min(old_size, new_size)):
             if old_commits_list[i] != new_commits_list[i]:
-                self.dataChanged.emit(self.index(self, i, 0),
-                                      self.index(self, i, self.columnCount(None)))
+                self.dataChanged.emit(self.index(i, 0),
+                                      self.index(i, self.columnCount(None)))
 
 
 
