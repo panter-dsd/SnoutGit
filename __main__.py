e67344eef7009d5b599360bc09ba51477304a891
PanteR <panter.dsd@gmail.com>
2012-11-26 12:49:33 +0400

    Using path from arguments.

--- __main__.py
+++ __main__.py
@@ -11,40 +11,38 @@ import diff_file_widget
 import commit_widget
 import actions_widget
 
-PATH = os.curdir
-
-
-@QtCore.Slot(str)
-def ttt(eee):
-    import commit
-    print(commit.Commit(PATH, eee).name())
-
 
 def main():
     """main"""
+
+    if len(sys.argv) < 2:
+        path = os.curdir
+    else:
+        path = sys.argv[1]
+
     app = QtGui.QApplication(sys.argv)
 
     window = QtGui.QMainWindow()
 
     commitesDock = QtGui.QDockWidget(window)
-    commites = commites_widget.CommitesWidget(PATH, commitesDock)
+    commites = commites_widget.CommitesWidget(path, commitesDock)
     commitesDock.setWidget(commites)
     window.addDockWidget(QtCore.Qt.TopDockWidgetArea, commitesDock)
 
     diffDock = QtGui.QDockWidget(window)
-    diff = diff_widget.DiffWidget(PATH, diffDock)
+    diff = diff_widget.DiffWidget(path, diffDock)
     diffDock.setWidget(diff)
     window.addDockWidget(QtCore.Qt.BottomDockWidgetArea, diffDock)
 
     commites.current_commit_changed.connect(diff.set_commit)
 
     status_dock = QtGui.QDockWidget(window)
-    status = status_widget.StatusWidget(PATH, status_dock)
+    status = status_widget.StatusWidget(path, status_dock)
     status_dock.setWidget(status)
     window.addDockWidget(QtCore.Qt.TopDockWidgetArea, status_dock)
 
     diff_file_dock = QtGui.QDockWidget(window)
-    diff_file = diff_file_widget.DiffFileWidget(PATH)
+    diff_file = diff_file_widget.DiffFileWidget(path)
     diff_file_dock.setWidget(diff_file)
     window.addDockWidget(QtCore.Qt.TopDockWidgetArea, diff_file_dock)
 
@@ -52,14 +50,14 @@ def main():
     status.status_changed.connect(diff_file.clear)
 
     commit_widget_dock = QtGui.QDockWidget(window)
-    commit = commit_widget.CommitWidget(PATH)
+    commit = commit_widget.CommitWidget(path)
     commit_widget_dock.setWidget(commit)
     window.addDockWidget(QtCore.Qt.BottomDockWidgetArea, commit_widget_dock)
 
     commit.commited.connect(commites.update_commites_list)
 
     actions_widget_dock = QtGui.QDockWidget(window)
-    actions = actions_widget.ActionsWidget(PATH)
+    actions = actions_widget.ActionsWidget(path)
     actions_widget_dock.setWidget(actions)
     window.addDockWidget(QtCore.Qt.BottomDockWidgetArea, actions_widget_dock)
 
