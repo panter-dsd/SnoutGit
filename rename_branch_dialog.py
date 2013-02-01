b05084322c52f2c58e74606afb5082a942ab5258
PanteR <panter.dsd@gmail.com>
2013-02-01 23:38:41 +0400

    RenameBranchDialog: some fixes.

--- rename_branch_dialog.py
+++ rename_branch_dialog.py
@@ -8,7 +8,7 @@ import git
 class RenameBranchDialog(QtGui.QDialog):
     _git = git.Git()
 
-    def __init__(self, parent=None):
+    def __init__(self, branch=str(), parent=None):
         super(RenameBranchDialog, self).__init__(parent)
 
         self._source_branch_label = QtGui.QLabel(self)
@@ -17,7 +17,9 @@ class RenameBranchDialog(QtGui.QDialog):
         self._source_branch = QtGui.QComboBox(self)
         self._source_branch.addItems(self._git.local_branches())
         self._source_branch.setCurrentIndex(
-            self._source_branch.find(self._git.current_branch())
+            self._source_branch.findText(
+                len(branch) > 0 and branch or self._git.current_branch()
+            )
         )
 
         self._target_branch_label = QtGui.QLabel(self)
@@ -38,6 +40,8 @@ class RenameBranchDialog(QtGui.QDialog):
                 len(self._target_branch.text()) > 0
             )
         )
+        self._target_branch.textChanged.emit(str())
+        self._target_branch.setFocus(8)
 
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self._source_branch_label)
@@ -45,6 +49,7 @@ class RenameBranchDialog(QtGui.QDialog):
         layout.addWidget(self._target_branch_label)
         layout.addWidget(self._target_branch)
         layout.addWidget(buttons)
+        super(RenameBranchDialog, self).setLayout(layout)
 
     def old_name(self):
         return self._source_branch.currentText()
