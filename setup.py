e4d2db48eebb722ccc938bdbb02edaaf442a7900
PanteR <panter.dsd@gmail.com>
2013-02-05 15:06:20 +0400

    Setup.py is added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SnoutGit',
+    version='0.0.0.0',
+    packages=[''],
+    url='https://github.com/panter-dsd/SnoutGit',
+    license='GPLv3',
+    author='PanteR',
+    author_email='panter.dsd@gmail.com',
+    description='Git gui client.'
+)
