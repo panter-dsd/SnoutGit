e08b35cca2aae683bb90e8c8f09bf89a054a59e5
PanteR <panter.dsd@gmail.com>
2013-04-29 10:53:20 +0400

    RenameBranchDialog: set_source_branch is added.

--- src/rename_branch_dialog.py
+++ src/rename_branch_dialog.py
@@ -9,18 +9,13 @@ class RenameBranchDialog(QtGui.QDialog):
     _git = git.Git()
 
     def __init__(self, branch=str(), parent=None):
-        super(RenameBranchDialog, self).__init__(parent)
+        super().__init__(parent)
 
         self._source_branch_label = QtGui.QLabel(self)
         self._source_branch_label.setText("Old name")
 
         self._source_branch = QtGui.QComboBox(self)
         self._source_branch.addItems(self._git.local_branches())
-        self._source_branch.setCurrentIndex(
-            self._source_branch.findText(
-                branch and branch or self._git.current_branch()
-            )
-        )
 
         self._target_branch_label = QtGui.QLabel(self)
         self._target_branch_label.setText("New name")
@@ -49,10 +44,17 @@ class RenameBranchDialog(QtGui.QDialog):
         layout.addWidget(self._target_branch_label)
         layout.addWidget(self._target_branch)
         layout.addWidget(buttons)
-        super(RenameBranchDialog, self).setLayout(layout)
+        self.setLayout(layout)
+
+        self.set_source_branch(branch)
 
     def old_name(self):
         return self._source_branch.currentText()
 
     def new_name(self):
         return self._target_branch.text()
+
+    def set_source_branch(self, branch):
+        self._source_branch.setCurrentIndex(
+            self._source_branch.findText(branch)
+        )
