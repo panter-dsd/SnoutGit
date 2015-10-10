aa7f685acd5e3b1ee99d7e5ae310f31dcc314229
ksk- <iksk810@yandex.ru>
2015-10-10 17:43:22 +0600

    Added class ApplicationSettings.

---
+++ src/ApplicationSettings.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import QSettings
+from PyQt5.QtWidgets import qApp
+
+
+class ApplicationSettings(object):
+    _application = qApp.applicationName()
+    _organization = qApp.organizationName()
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super().__new__(cls)
+
+        return cls.instance
+
+    def value(self, key, default_value, scope=QSettings.UserScope):
+        return self._settings(scope).value(key, default_value)
+
+    def set_value(self, key, value, scope=QSettings.UserScope):
+        self._settings(scope).setValue(key, value)
+
+    def _settings(self, scope):
+        return QSettings(scope, self._organization, self._application)
+
+
+application_settings = ApplicationSettings()
